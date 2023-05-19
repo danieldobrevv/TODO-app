1f9f48498fad50d7b00e526b27beec59c3a23976
haygrouve <haygrouve@gmail.com>
2023-05-19 17:20:54 +0300

    todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTodoList([...todoList, todo])
+    setTodo("")
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> My TODO </h1>
+      <h3> My TODO </h3>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          value={todo}
+          placeholder="Write your task"
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        ></input>
+      </form>
+      {todoList.map((task, index) => {
+        return <p key={index}>{task}</p>;
+      })}
     </div>
   );
 }
